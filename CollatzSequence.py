afbc23c3da45887a3515f6f68d1fdee7f3def86d
Md Misbahul Islam (Saad) <70517174+misbahulsaad@users.noreply.github.com>
2023-12-19 22:16:50 +0600

    Update CollatzSequence.py
    Added integer only input

--- CollatzSequence.py
+++ CollatzSequence.py
@@ -1,13 +1,16 @@
-def collatz(number):
-    return calculation(number)
+try:
+    def collatz(number):
+        return calculation(number)
 
-def calculation(number):
-    if number % 2 == 0 :
-        return number // 2
-    else:
-        return 3 * number + 1
+    def calculation(number):
+        if number % 2 == 0 :
+            return number // 2
+        else:
+            return 3 * number + 1
 
-number = int(input('Enter a number: '))
-while number != 1:
-    print(collatz(number))
-    number = collatz(number)
+    number = int(input('Enter a number: '))
+    while number != 1:
+        print(collatz(number))
+        number = collatz(number)
+except ValueError:
+    print('You must enter an integer')
