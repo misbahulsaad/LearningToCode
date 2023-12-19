a3888dcf59fe4e7f7cf3d66be1a20f83bc2411b5
Md Misbahul Islam (Saad) <70517174+misbahulsaad@users.noreply.github.com>
2023-12-19 16:20:45 +0600

    Create CollatzSequence.py

---
+++ CollatzSequence.py
@@ -0,0 +1,13 @@
+def collatz(number):
+    return calculation(number)
+
+def calculation(number):
+    if number % 2 == 0 :
+        return number // 2
+    else:
+        return 3 * number + 1
+
+number = int(input('Enter a number: '))
+while number != 1:
+    print(collatz(number))
+    number = collatz(number)
